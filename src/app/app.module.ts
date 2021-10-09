9850668d8ad9bfc19f99ba0eeeaf8c82e43c0e89
vishal-08 <66135224+vishal-08@users.noreply.github.com>
2021-10-09 12:01:05 +0530

    first commit component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { headerComponent } from './header/header.component';
+import { ReceipesComponent } from './receipes/receipes.component';
+import { ReceipeListComponent } from './receipes/receipe-list/receipe-list.component';
+import { ReceipeDetailComponent } from './receipes/receipe-detail/receipe-detail.component';
+
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { ReceipeItemComponent } from './receipes/receipe-list/receipe-item/receipe-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    headerComponent,
+    ReceipesComponent,
+    ReceipeListComponent,
+    ReceipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    ReceipeItemComponent
   ],
   imports: [
     BrowserModule,
