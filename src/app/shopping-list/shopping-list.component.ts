240bc109926a705a0ef7de52278add9640f6f620
vishal-08 <66135224+vishal-08@users.noreply.github.com>
2021-10-09 16:05:03 +0530

    populated all the components and models

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredients } from '../shared/ingredients.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients : Ingredients[] =  [
+    new Ingredients("Apple", 6),
+    new Ingredients("Green Chilli", 2),
+    new Ingredients("Mango", 3)
+  ]
 
   constructor() { }
 
